c6d34d809dd670910d45d019c776d82fa2b36038
A C S C WIJAYARTHNE <KUDSE222F-062@student.nibm.lk>
2023-12-15 10:49:26 +0530

    updated

--- epitome_hotel/src/pages/Register.jsx
+++ epitome_hotel/src/pages/Register.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+
+import "../styles/login.css";
+
+
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <section className="login">
+    <div className="wrapper2">
+      <form action="">
+        <h1>Register</h1>
+        <div className="input-box">
+          <input type="text" name="" id="" placeholder='Username' required/>
+          <i class="icon ri-user-2-fill"></i>
+        </div>
+        <div className="input-box">
+          <input type="fname" name="" id="" placeholder='First Name' required/>
+          <i class="icon ri--2-fill"></i>
+        </div>
+        <div className="input-box">
+          <input type="lname" name="" id="" placeholder='Last Name' required/>
+          <i class="icon ri--2-fill"></i>
+        </div>
+        <div className="input-box">
+          <input type="password" name="" id="" placeholder='Password' required/>
+          <i class="icon ri-lock-2-fill"></i>
+        </div>
+          <div className="input-box">
+          <input type="password" name="" id="" placeholder='Conform Password' required/>
+          <i class="icon ri-lock-2-fill"></i>
+        </div>
+        <div className="input-box">
+          <div className="input-box">
+          <input type="text" name="" id="" placeholder='Role' required/>
+          <i class="icon ri-user-2-fill"></i>
+          </div>
+        </div>
+        
+        <button type='submit'>Register</button>
 
-export default Register
+        <div className="register-link">
+          <p>Already have an account? <a href="#">Log In</a></p>
+        </div>
+      </form>
+    </div>
+  </section>
+  );
+};
+
+export default Register;
