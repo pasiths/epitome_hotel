75a72e6cc7de128ed01bf8ce6909414fcf8082b9
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-12-15 07:16:51 +0530

    Modified Login.jsx

--- epitome_hotel/src/pages/Login.jsx
+++ epitome_hotel/src/pages/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import '../styles/login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className="login">
+      <div className="wrapper">
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" name="" id="" placeholder='Username' required/>
+            <i class="icon ri-user-2-fill"></i>
+          </div>
+          <div className="input-box">
+            <input type="password" name="" id="" placeholder='Password' required/>
+            <i class="icon ri-lock-2-fill"></i>
+          </div>
+
+          <div className="remember-forgot">
+            <label htmlFor="">
+              <input type="checkbox" name="" id="" />
+              Remember me
+            </label>
+            <a href="#">Forget Password</a>
+          </div>
+          
+          <button type='submit'>Login</button>
+
+          <div className="register-link">
+            <p>Don't have an account? <a href="#">Register</a></p>
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
 
