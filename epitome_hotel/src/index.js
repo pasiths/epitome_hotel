5a4959e4ac427fda01310a000a2e88eaf4902f10
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-12-20 21:48:03 +0530

    Modified Login.jsx and add AuthContext.js, userFetch.js

--- epitome_hotel/src/index.js
+++ epitome_hotel/src/index.js
@@ -7,17 +7,17 @@ import "remixicon/fonts/remixicon.css";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
-// import { AuthContextProvider } from "./context/AuthContext";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import { AuthContextProvider } from "./context/AuthContext";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <AuthContextProvider> */}
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthContextProvider>
         <App />
-      </BrowserRouter>
-    {/* </AuthContextProvider> */}
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
