8d005abadd4ed4eb9eb70a24f7c1f06d7a7a5343
A C S C WIJAYARTHNE <KUDSE222F-062@student.nibm.lk>
2023-12-20 23:32:06 +0530

    updated

--- epitome_hotel/src/pages/Login.jsx
+++ epitome_hotel/src/pages/Login.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import '../styles/login.css'
-import Header from '../components/Header/Header'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import "../styles/login.css";
+import Header from "../components/Header/Header";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -21,7 +23,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      let role = null;
+      let role = null || "";
       const res = await axios.post("/auth/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       role = user.role;
@@ -38,43 +40,79 @@ const Login = () => {
   console.log(user);
   return (
     <>
-    <Header/>
-    <section className="login">
-      <div className="wrapper">
-        <form action="">
-          <h1>Login</h1>
-          <div className="input-box">
-            <input type="text" name="" id="" placeholder='Username' required/>
-            <i class="icon ri-user-2-fill"></i>
-          </div>
-          <div className="input-box">
-            <input type="password" name="" id="" placeholder='Password' required/>
-            <i class="icon ri-lock-2-fill"></i>
-          </div>
+      <Header />
+      <section className="login">
+        <div className="wrapper">
+          <form action="">
+            <h1>Login</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                name=""
+                id="username"
+                placeholder="Username"
+                onChange={handleChange}
+                required
+              />
+              <i class="icon ri-user-2-fill"></i>
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                name=""
+                id="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
+              <i class="icon ri-lock-2-fill"></i>
+            </div>
 
-          <div className="remember-forgot">
-            <label htmlFor="">
-              <input type="checkbox" name="" id="" />
-              Remember me
-            </label>
-            <a href="#">Forget Password</a>
-          </div>
-          
-          <button type='submit'>Login</button>
+            <div className="remember-forgot">
+              <label htmlFor="">
+                <input type="checkbox" name="" id="" />
+                Remember me
+              </label>
+              <a href="#">Forget Password</a>
+            </div>
 
-          <div className="register-link">
-            <p>Don't have an account? <a href="#" >Register</a></p>
-          </div>
-        </form>
-      </div>
-    </section>
-    <button><Link to={"/adminpanel"}>Admin panel</Link></button>
-    <button><Link to={"/cloud9cafehome"}>Cloud 9 Cafe</Link></button>
-    <button><Link to={"/rooftoppoolbarhome"}>Rooftop poolbar</Link></button>
-    <button><Link to={"/feast365resturenthome"}>Feast 365 Restaurent</Link></button>
-    <button><Link to={"/coldkitchen"}>Cold Kitchen</Link></button>
-    <button><Link to={"/hotkitchen"}>Hot Kitchen</Link></button>
-    <button><Link to={"/mainbar"}>Main bar</Link></button>
+            <button type="submit" onClick={handleClick}>
+              Login
+            </button>
+            {error && <span>{error.message}</span>}
+
+            <div className="register-link">
+              <p>
+                Don't have an account?{" "}
+                <Link to={"/Register"} style={{ color: "black" }}>
+                Register
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+      <button>
+        <Link to={"/adminpanel"}>Admin panel</Link>
+      </button>
+      <button>
+        <Link to={"/cloud9cafehome"}>Cloud 9 Cafe</Link>
+      </button>
+      <button>
+        <Link to={"/rooftoppoolbar"}>Rooftop poolbar</Link>
+      </button>
+      <button>
+        <Link to={"/feast365resturenthome"}>Feast 365 Restaurent</Link>
+      </button>
+      <button>
+        <Link to={"/coldkitchen"}>Cold Kitchen</Link>
+      </button>
+      <button>
+        <Link to={"/hotkitchen"}>Hot Kitchen</Link>
+      </button>
+      <button>
+        <Link to={"/mainbar"}>Main bar</Link>
+      </button>
     </>
   );
 };
--- epitome_hotel/src/pages/Register.jsx
+++ epitome_hotel/src/pages/Register.jsx
@@ -62,10 +62,17 @@ const Register = () => {
               />
               <i class="icon ri-lock-2-fill"></i>
             </div>
-            <div className="input-box">
-              <input type="text" name="" id="" placeholder="Role" required />
-              <i class="icon ri-user-2-fill"></i>
-            </div>
+            <select name="role" id="role" className="input-box" style={{ border:"solid" , borderRadius: 30, paddingLeft: 20, paddingRight: 40, borderColor: "#faa935" , marginTop:14 ,marginBottom:14 }}>
+              <option value="">Role</option>
+              <option value="Admin">Admin</option>
+              <option value="poolbarUser">Poolbar User</option>
+              <option value="cloud9cafeUser">Cloud 9 cafe User</option>
+              <option value="MainRestaurent">Main Restaurent User</option>
+              <option value="hotKitchenUser">Hot Kitchen User</option>
+              <option value="coldKitchenUser">Cold Kitchen User</option>
+              <option value="mainBarUser">Main Bar User</option>
+            </select>
+
             <div className="input-box">
               <input
                 type="text"
