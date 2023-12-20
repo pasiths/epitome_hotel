4e15409c88ca02450209f776731cfd16d50b1f02
A C S C WIJAYARTHNE <KUDSE222F-062@student.nibm.lk>
2023-12-20 22:42:32 +0530

    updated

--- epitome_hotel/src/pages/Login.jsx
+++ epitome_hotel/src/pages/Login.jsx
@@ -2,48 +2,56 @@ import React from 'react'
 import '../styles/login.css'
 import Header from '../components/Header/Header'
 import { Link } from 'react-router-dom'
+import { Container } from 'reactstrap'
 
 const Login = () => {
 
   return (
     <>
-    <Header/>
-    <section className="login">
-      <div className="wrapper">
-        <form action="">
-          <h1>Login</h1>
-          <div className="input-box">
-            <input type="text" name="" id="" placeholder='Username' required/>
-            <i class="icon ri-user-2-fill"></i>
-          </div>
-          <div className="input-box">
-            <input type="password" name="" id="" placeholder='Password' required/>
-            <i class="icon ri-lock-2-fill"></i>
-          </div>
+      <Header />
+      <section className="login">
+        <div className="wrapper">
+          <form action="">
+            <h1>Login</h1>
+            <div className="input-box">
+              <input type="text" name="" id="" placeholder='Username' required />
+              <i class="icon ri-user-2-fill"></i>
+            </div>
+            <div className="input-box">
+              <input type="password" name="" id="" placeholder='Password' required />
+              <i class="icon ri-lock-2-fill"></i>
+            </div>
 
-          <div className="remember-forgot">
-            <label htmlFor="">
-              <input type="checkbox" name="" id="" />
-              Remember me
-            </label>
-            <a href="#">Forget Password</a>
-          </div>
+            <div className="remember-forgot">
+              <label htmlFor="">
+                <input type="checkbox" name="" id="" />
+                Remember me
+              </label>
+              <a href="#">Forget Password</a>
+            </div>
+
+            <button type='submit' style={{color:'white'}}>Login</button>
+
+            <div className="register-link">
+              <p>Don't have an account? <a href="#" >Register</a></p>
+            </div>
+          </form>
+        </div>
+      </section>
+      <Container>
+      <center>
+        <div className='wrapper'>
+          <button type='submit' style={{marginBottom:8}}><Link to={"/adminpanel"} style={{color:'white'}}>Admin panel</Link></button>
+          <button type='submit' style={{marginBottom:8}}><Link to={"/cloud9cafehome"} style={{color:'white'}}>Cloud 9 Cafe</Link></button>
+          <button type='submit' style={{marginBottom:8}}><Link to={"/rooftoppoolbarhome"} style={{color:'white'}}>Rooftop poolbar</Link></button>
+          <button type='submit' style={{marginBottom:8}}><Link to={"/feast365resturenthome"} style={{color:'white'}}>Feast 365 Restaurent</Link></button>
+          <button type='submit' style={{marginBottom:8}}><Link to={"/coldkitchen"} style={{color:'white'}}>Cold Kitchen</Link></button>
+          <button type='submit' style={{marginBottom:8}}><Link to={"/hotkitchen"} style={{color:'white'}}>Hot Kitchen</Link></button>
+          <button type='submit' style={{marginBottom:8}}><Link to={"/mainbar"} style={{color:'white'}}>Main bar</Link></button>
           
-          <button type='submit'>Login</button>
-
-          <div className="register-link">
-            <p>Don't have an account? <a href="#" >Register</a></p>
-          </div>
-        </form>
-      </div>
-    </section>
-    <button><Link to={"/adminpanel"}>Admin panel</Link></button>
-    <button><Link to={"/cloud9cafehome"}>Cloud 9 Cafe</Link></button>
-    <button><Link to={"/rooftoppoolbarhome"}>Rooftop poolbar</Link></button>
-    <button><Link to={"/feast365resturenthome"}>Feast 365 Restaurent</Link></button>
-    <button><Link to={"/coldkitchen"}>Cold Kitchen</Link></button>
-    <button><Link to={"/hotkitchen"}>Hot Kitchen</Link></button>
-    <button><Link to={"/mainbar"}>Main bar</Link></button>
+        </div>
+        </center>
+      </Container>
     </>
   )
 }
